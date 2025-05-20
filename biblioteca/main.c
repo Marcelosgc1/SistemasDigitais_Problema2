d60386dfbebf6ad69bfdd9bf8765fe81587fe266
Marcelosgc1 <marcelobtsgc1@gmail.com>
2025-05-20 20:28:50 -0300

    :construction: tests: testes p/ demonstração do funcionamento do código

--- biblioteca/main.c
+++ biblioteca/main.c
@@ -3,288 +3,316 @@
 #include "dafema.h"
 #include "txt.h"
 
-void new5(endereco *adrs);
-void escrever5x5(endereco *adrs);
-void escrever4x4(endereco *adrs);
-void escrever3x3(endereco *adrs);
-void escrever2x2(endereco *adrs);
-int iniciarPrograma(int seletor, signed char n0, signed char n1);
+
+void escreveMatriz(int matriz[][5], int tamanho, int matrizId){
+    for (int i = 0; i < tamanho; i++){
+        for (int j = 0; j < tamanho; j++){
+            escreverIndice(matriz[i][j], matrizId, i, j);
+        }
+    }
+}
+
+void leMatriz(int matriz[][5], int tamanho, int matrizId){
+    for (int i = 0; i < tamanho; i++){
+        for (int j = 0; j < tamanho; j++){
+            matriz[i][j] = lerIndice(matrizId, i, j);
+        }
+    }
+}
+
+void printMatriz(int matriz[][5], int tamanho){
+    printf("Imprimindo matriz %dx%d:\n",tamanho,tamanho);
+    for (int i = 0; i < tamanho; i++){
+        for (int j = 0; j < tamanho; j++){
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+void operacoes(){
+    int m[5][5];
+    soma();
+    getchar();
+    leMatriz(m,5,2);
+    printf("SOMA\n");
+    printMatriz(m, 5);
+
+    subtracao();
+    getchar();
+    leMatriz(m,5,2);
+    printf("SUBTRACAO\n");
+    printMatriz(m, 5);
+    
+    transposicao();
+    getchar();
+    leMatriz(m,5,2);
+    printf("TRANSPOSICAO\n");
+    printMatriz(m, 5);
+    
+    oposta();
+    getchar();
+    leMatriz(m,5,2);
+    printf("OPOSTA\n");
+    printMatriz(m, 5);
+    
+    multiEscalar(4);
+    getchar();
+    leMatriz(m,5,2);
+    printf("MUL ESCALAR\n");
+    printMatriz(m, 5);
+    
+    multiplicacao();
+    getchar();
+    leMatriz(m,5,2);
+    printf("MULTIPLICACAO\n");
+    printMatriz(m, 5);
+    
+    determinante2x2();
+    getchar();
+    leMatriz(m,5,2);
+    printf("DETERMINANTE 2X2\n");
+    printMatriz(m, 5);
+    
+    determinante3x3();
+    getchar();
+    leMatriz(m,5,2);
+    printf("DETERMINANTE 3X3\n");
+    printMatriz(m, 5);
+
+    determinante4x4();
+    getchar();
+    leMatriz(m,5,2);
+    printf("DETERMINANTE 4X4\n");
+    printMatriz(m, 5);
+
+    determinante5x5();
+    getchar();
+    leMatriz(m,5,2);
+    printf("DETERMINANTE 5X5\n");
+    printMatriz(m, 5);
+
+}
+
+void escrever2x2ASM(){
+    __uint8_t temp;
+    signed char n0, n1;
+
+    endereco *adrs = malloc(sizeof(signed char) * 3);
+    *adrs = (endereco) {0,0,0};
+    
+    printf("Escreva [0][0]: ");
+    scanf("%d", &temp);
+    n0 = temp;
+    printf("Escreva [0][1]: ");
+    scanf("%d", &temp);
+    n1 = temp;
+
+    escreverASM(n0, n1, adrs);
+    adrs->coluna = 4;
+    
+    printf("Escreva [1][0]: ");
+    scanf("%d", &temp);
+    n1 = temp;
+
+    escreverASM(0, n1, adrs);
+    adrs->coluna = 1;
+    adrs->linha = 1;
+
+    printf("Escreva [1][1]: ");
+    scanf("%d", &temp);
+    n0 = temp;
+
+    escreverASM(n0, 0, adrs);
+
+}
+
+void ler2x2ASM(int matriz[2][5]){
+    endereco *adrs = malloc(sizeof(signed char) * 3);
+    *adrs = (endereco) {0,0,0};
+    int temp;
+    
+    temp = lerASM(adrs);
+    matriz[0][0] = temp & 255;
+    matriz[0][1] = temp >> 8;
+
+    adrs->coluna = 4;
+    temp = lerASM(adrs);
+    matriz[1][0] = temp >> 8;
+
+    adrs->linha = 1;
+    adrs->coluna = 1;
+    temp = lerASM(adrs);
+    matriz[1][1] = temp & 255;
+
+}
+
+void exemplo1(){
+    int a[2][5]= {
+        {2, 3},
+        {7,-4}
+    };
+
+    int b[2][5]= {
+        {0, 0},
+        {0, 0}
+    };
+
+    printf("Matriz esperada 2x2: \n");
+    printMatriz(a, 2);
+    escreveMatriz(a, 2, 0);
+    leMatriz(b, 2, 0);
+    printMatriz(b, 2);
+}
+
+void exemplo2(){
+    int a[3][5]= {
+        {2, 3, -1},
+        {7,-4, -1},
+        {20, 1, -9}
+    };
+
+    int b[3][5]= {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+
+    printf("Matriz esperada 3x3: \n");
+    printMatriz(a, 3);
+    escreveMatriz(a, 3, 0);
+    leMatriz(b, 3, 0);
+    printMatriz(b, 3);
+}
+
+void exemplo3(){
+    int a[4][5]= {
+        {2, 3, -1, 11},
+        {7,-4, -1, 20},
+        {20, 1, -9, 6},
+        {0, 54, -32, 7}
+
+    };
+
+    int b[4][5]= {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    };
+    printf("Matriz esperada 4x4: \n");
+    printMatriz(a, 4);
+    escreveMatriz(a, 4, 0);
+    leMatriz(b, 4, 0);
+    printMatriz(b, 4);
+}
+
+void exemplo4(){
+    int a[5][5]= {
+        {2, 3, -1, 11, 22},
+        {7,-4, -1, 20, 1},
+        {20, 1, -9, 6, 0},
+        {0, 54, -32, 7, 8},
+        {-3, 5, 1, 27, -1}
+
+    };
+
+    int b[5][5]= {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    printf("Matriz esperada 5x5: \n");
+    printMatriz(a, 5);
+    escreveMatriz(a, 5, 0);
+    leMatriz(b, 5, 0);
+    printMatriz(b, 5);
+}
+
+void exemplo5(){
+    int matriz[2][5];
+    escrever2x2ASM();
+    ler2x2ASM(matriz);
+    printMatriz(matriz, 2);
+
+}
+
+void exemplo6(){
+    int a[5][5]= {
+        {2, 3, -1, 11, 22},
+        {7,-4, -1, 20, 1},
+        {20, 1, -9, 6, 0},
+        {0, 54, -32, 7, 8},
+        {-3, 5, 1, 27, -1}
+    };
+
+    int b[5][5]= {
+        {12, 4, -5, 7, 3},
+        {7,-4, 11, 0, -20},
+        {2, 13, 2, 6, 12},
+        {0, 21, 12, 7, 5},
+        {-6, 2, 2, 7, -1}
+    };
+
+    printf("matriz A:\n");
+    escreveMatriz(a,5,0);
+    printMatriz(a,5);
+    printf("matriz B:\n");
+    escreveMatriz(b,5,1);
+    printMatriz(b,5);
+    getchar();
+    operacoes();
+}
+
+void exemplo7(){
+    ler(1,0,1);
+}
 
 int main(){
-	printf("a\n");
 	iniciarDafema();
-	printf("b\n");
-	//soma();
-	printf("c\n");
+    int opcao = 1, delay;
 
-	signed char n0 = 0;
-	signed char n1 = 1;
-	int seletor = 0;
-	int x = 1;
-	while (x != 0) {
-		x = iniciarPrograma(seletor, n0, n1);
-	}
+    while(opcao != 0){
+        printf("[1] Matrizes pre-setadas\n");
+        printf("[2] Escrever Matriz pelo terminal\n");
+        printf("[3] Exit call p/ erro\n");
+        printf("[5] Testar Operações\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
 
+        switch (opcao){
+        case 1:
+            getchar();
+            exemplo1();
+            getchar();
+            exemplo2();
+            getchar();
+            exemplo3();
+            getchar();
+            exemplo4();
+            break;
+
+        case 2:
+            exemplo5();
+            break;
+            
+        case 3:
+            exemplo7();
+            break;
+        case 5:
+            exemplo6();
+            break;
+        default:
+            break;
+        }
+    }
+    
     encerrarDafema();
 	return 0;
 };
 
-
-int iniciarPrograma(int seletor, signed char n0, signed char n1) {
-    printf("\nDIGITE A OPERAÇÃO DESEJADA: ");
-    printf("\n[1] LEITURA: ");
-    printf("\n[2] ESCRITA: ");
-    printf("\n[3] SOMA: ");
-    printf("\n[4] SUBTRAÇÃO: ");
-    printf("\n[5] MULTIPLICAÇÃO: ");
-    printf("\n[6] TRANSPOSTA: ");
-    printf("\n[7] OPOSTA: ");
-    printf("\n[8] MULTIPLICAÇÃO ESCALAR: ");
-    printf("\n[9] DETER2X2: ");
-    printf("\n[10] DETER3X3: ");
-    printf("\n[11] DETER4X4: ");
-    printf("\n[12] DETER5X5: ");
-    printf("\n[13] LER RESULT: ");
-    printf("\n[14] ESCREVER MATRIZ COMPLETA: ");
-    printf("\n[15] ALTERAR ID, COLUNA E LINHA: ");
-    printf("\n[16] TESTE DE ERRO: ");
-    printf("\n[17] LER MATRIZ COMPLETA: ");
-    printf("\n OPÇÃO: ");
-    int temporario, size;
-    signed char temp1, temp2, temp3;
-    scanf("%d", &seletor);
-    endereco *meuEndereco = malloc(sizeof(endereco));
-    // meuEndereco->matriz_id = 1;
-    // meuEndereco->linha = 0;
-    // meuEndereco->coluna = 2;
-
-
-    switch (seletor) {
-        case 1:
-            *meuEndereco = (endereco) {0,0,0};
-            temp1 = ler(meuEndereco);
-	    //printf("aaa\n")
-            temp2 = lerIndice(meuEndereco);
-            meuEndereco->coluna = 1;
-            temp3 = lerIndice(meuEndereco);
-
-            printf("pacote de dados: %d\n", temp1);
-            printf("valor n0: %d\n", temp2);
-            printf("valor n1: %d\n", temp3);
-	    break;
-        case 2:
-            printf("valor n0: ");
-            scanf("%d", &temp1); 
-            printf("valor n1: ");
-            scanf("%d", &temporario);
-            temp2 = (signed char) temporario;
-
-            *meuEndereco = (endereco) {0,0,2};
-            escrever(temp1, temp2, meuEndereco);
-
-            *meuEndereco = (endereco) {1,0,2};
-            escrever(temp1, temp2, meuEndereco);
-            //enviarMatriz(n0, n1, 0, addressA, 2); // Escrevendo números na matriz A
-            //enviarMatriz(n0, n1, 1, addressB, 2); // Escrevendo números na matriz B
-            break;
-        case 3:
-            soma();     
-            break;
-        case 4:
-            subtracao(); 
-            break;
-        case 5:
-            multiplicacao(); 
-            break;
-        case 6:
-            transposicao();    
-            break;
-        case 7:
-            oposta();
-            break;
-        case 8:
-            multiEscalar(5); 
-            break;
-        case 9:
-            determinante2x2();     
-            break;
-        case 10:
-            determinante3x3();     
-            break;
-        case 11:
-            determinante4x4();     
-            break;
-        case 12:
-            determinante5x5();    
-            break;
-        case 13:
-            *meuEndereco = (endereco) {2,0,0};
-            temp1 = ler(meuEndereco);
-
-            temp2 = lerIndice(meuEndereco);
-            
-            *meuEndereco = (endereco) {2,0,1};
-            temp3 = lerIndice(meuEndereco);
-
-            printf("pacote de dados: %d\n", temp1);
-            printf("valor n0: %d\n", temp2);
-            printf(">linha < 5 && adrs->coluna < 5valor n1: %d\n", temp3);
-	        break;
-        case 14:
-            scanf("%d", &size);
-            switch(size){
-                case 5:
-                    *meuEndereco = (endereco) {0,0,0};
-                    escrever5x5(meuEndereco);
-                    *meuEndereco = (endereco) {1,0,0};
-                    escrever5x5(meuEndereco);
-                    break;
-                case 4:
-                    *meuEndereco = (endereco) {0,0,0};
-                    escrever4x4(meuEndereco);
-                    meuEndereco->matriz_id = 1;
-                    escrever4x4(meuEndereco);
-                    break;
-                case 3:
-                    *meuEndereco = (endereco) {0,0,0};
-                    escrever3x3(meuEndereco);
-                    meuEndereco->matriz_id = 1;
-                    escrever3x3(meuEndereco);
-                    break;
-                case 2:
-                    *meuEndereco = (endereco) {0,0,0};
-                    escrever4x4(meuEndereco);
-                    meuEndereco->matriz_id = 1;
-                    escrever4x4(meuEndereco);
-                    break;
-                default:
-                    printf("Só tamanho 2, 3, 4 ou 5");
-                    break;
-            }
-            break;
-	    case 15:
-            new5(meuEndereco);
-            break;
-        case 16:
-            // Teste de Erro ao apontar para um ID de matriz inválido
-            *meuEndereco = (endereco) {4, 0, 0};
-            escrever3x3(meuEndereco);
-            break;
-        case 17:
-            // Lendo Matriz A
-            for(char i =0; i < 5; i++){
-                for(char j = i % 2; j < 5; j+=2){
-                    *meuEndereco = (endereco) {0, i, j};
-                    temp2 = lerIndice(meuEndereco);
-                    temp3 = lerIndice(meuEndereco);
-                    printf("valor n0: %d\n", temp2);
-                    printf("valor n1: %d\n", temp3);
-                }
-            }
-            break;
-        default:
-            printf("Opção inválida! Tente novamente.\n");
-            break;
-    }
-    return seletor;
-}
-
-
-void escrever5x5(endereco *adrs){
-    signed char *dados = ler_arquivo_txt("teste.txt");
-
-    int n = 0;
-    do{
-        escrever(dados[n], dados[n+1], adrs);
-        printf("%d\n\n",dados[n]);
-        n = n + 2;
-        adrs->coluna = adrs->coluna + 2;
-        if (adrs->coluna > 4){
-            adrs->coluna = adrs->coluna % 5;
-            adrs->linha = adrs->linha + 1;
-        }
-        printf("%d/%d\n", adrs->linha, adrs->coluna);
-    }while (adrs->linha < 5 && adrs->coluna < 5);
-    adrs->coluna = 0;
-    adrs->linha = 0;  
-
-    liberar_lista(dados);
-}
-
-void escrever4x4(endereco *adrs){
-    signed char *dados = ler_arquivo_txt("teste.txt");
-
-    int n = 0;
-    do{
-        escrever(dados[n], dados[n+1], adrs);
-        printf("%d\n\n",dados[n]);
-        n = n + 2;
-        adrs->coluna = adrs->coluna + 2;
-        if (adrs->coluna > 3){
-            adrs->coluna = adrs->coluna % 5;
-            adrs->linha = adrs->linha + 1;
-        }
-        printf("%d/%d\n", adrs->linha, adrs->coluna);
-    }while (adrs->linha < 4 && adrs->coluna < 4);
-    adrs->coluna = 0;
-    adrs->linha = 0;  
-
-    liberar_lista(dados);
-}
-
-void escrever3x3(endereco *adrs){
-    signed char *dados = ler_arquivo_txt("teste.txt");
-
-    int n = 0;
-    do{
-        escrever(dados[n], dados[n+1], adrs);
-        printf("%d\n\n",dados[n]);
-        n = n + 2;
-        adrs->coluna = adrs->coluna + 2;
-        if (adrs->coluna > 2){
-            adrs->coluna = adrs->coluna % 5;
-            adrs->linha = adrs->linha + 1;
-        }
-        printf("%d/%d\n", adrs->linha, adrs->coluna);
-    }while (adrs->linha < 3 && adrs->coluna < 3);
-    adrs->coluna = 0;
-    adrs->linha = 0;  
-
-    liberar_lista(dados);
-}
-
-void escrever2x2(endereco *adrs){
-    signed char *dados = ler_arquivo_txt("teste.txt");
-
-    int n = 0;
-    do{
-        escrever(dados[n], dados[n+1], adrs);
-        printf("%d\n\n",dados[n]);
-        n = n + 2;
-        adrs->coluna = adrs->coluna + 2;
-        if (adrs->coluna > 1){
-            adrs->coluna = adrs->coluna % 5;
-            adrs->linha = adrs->linha + 1;
-        }
-        printf("%d/%d\n", adrs->linha, adrs->coluna);
-    }while (adrs->linha < 2 && adrs->coluna < 2);
-    adrs->coluna = 0;
-    adrs->linha = 0;  
-
-    liberar_lista(dados);
-}
-
-void new5(endereco *adrs){
-    *adrs = (endereco) {1,0,0};
-    int temp;
-    for (int i = 0; i < 5; i++){
-        for (int j = 0; j < 5; j++){
-            scanf("%d", &temp);
-	        signed char x = (signed char) temp;
-            *adrs = (endereco) {1,i,j};
-            escreverIndice(x, adrs);
-        }
-    }
-    *adrs = (endereco) {0,0,0};
-    return;
-}
